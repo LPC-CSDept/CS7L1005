4063ace94b6b2abd4a17314b713522dec3f92089
Kyu Lee <kwooler@gmail.com>
2023-06-09 17:41:00 -0700

    L1005

--- main.py
+++ main.py
@@ -1,29 +1,62 @@
 
 class Student:
-    #########################################
-    # Code your program here
-    #########################################
+    numofStudent = 0
+
+    def __init__(self, sid, name):
+        self._id = sid
+        self._name = name
+        Student.numofStudent += 1
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        self._id = id
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    def __del__(self):
+        Student.numofStudent -= 1
+        del self
+
+    def __str__(self):
+        return (f'Student id: {self.id:>10} \t name: {self.name:>10}')
 
 
 def makeStudent(student_dict_list):
-    #########################################
-    # Code your program here
-    #########################################
+    slist = []
+    for student in student_dict_list:
+        sid = student['id']
+        name = student['name']
+        s = Student(sid, name)
+        slist.append(s)
+    return slist
 
 
 def deleteOneStudent(slist, did):
-    #########################################
-    # Code your program here
-    #########################################
+    for i, student in enumerate(slist):
+        if student.id == did:
+            print('-->Deleted', student)
+            del student
+            del slist[i]
+            # Student.numofStudent -= 1
+
 
 def main():
+
     student_dict_list = [{'id': 1001, 'name': 'John'},
                          {'id': 1002, 'name': 'James'},
                          {'id': 1003, 'name': 'Mark'},
                          {'id': 1004, 'name': 'Matthew'},
                          {'id': 1005, 'name': 'Arnold'}]
-
-    # makeStudent
     slist = makeStudent(student_dict_list)
     print(f'\n**** Total number of students: {Student.numofStudent}')
     for s in slist:
--- main_test.py
+++ main_test.py
@@ -7,10 +7,6 @@ import types
 
 
 def test_main_1():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    # datastr = 'Kim \n 100 80 70 60\n Bill \n 100 90 80 60 \n Mary \n 90 80 70 100'
-    # sys.stdin = io.StringIO(datastr)
 
     student_dict_list = [{'id': 1001, 'name': 'John'},
                          {'id': 1002, 'name': 'James'},
@@ -22,19 +18,24 @@ def test_main_1():
     for s in slist:
         print(s)
 
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
     assert len(slist) == 5, "Length must be 5"
     assert main.Student.numofStudent == 5, 'Class variable numofStudent must be 5'
     assert slist[0].id == 1001
     assert slist[4].id == 1005
 
+
+def test_main_2():
+    student_dict_list = [{'id': 1001, 'name': 'John'},
+                         {'id': 1002, 'name': 'James'},
+                         {'id': 1003, 'name': 'Mark'},
+                         {'id': 1004, 'name': 'Matthew'},
+                         {'id': 1005, 'name': 'Arnold'}]
+    slist = main.makeStudent(student_dict_list)
     main.deleteOneStudent(slist, 1003)
     assert len(slist) == 4, 'Length must be 4'
     assert main.Student.numofStudent == 4, 'Class variable numofStudent must be 4'
+
+    slist.sort(key=lambda x: x.id)
     assert slist[2].id == 1004
 
     print(f'\n**** Total number of students: {main.Student.numofStudent}')
@@ -48,3 +49,25 @@ def test_main_1():
     # res = re.search(regex_string, lines[1])
     # assert res != None
     # print(res.group())
+
+
+def test_main_3():
+    student_dict_list = [{'id': 1001, 'name': 'John'},
+                         {'id': 1002, 'name': 'James'},
+                         {'id': 1003, 'name': 'Mark'},
+                         {'id': 1004, 'name': 'Matthew'},
+                         {'id': 1005, 'name': 'Arnold'}]
+    slist = main.makeStudent(student_dict_list)
+
+    didlist = []
+    for d in student_dict_list:
+        didlist.append(d['id'])
+
+    for idnum in didlist:
+        main.deleteOneStudent(slist, idnum)
+    assert len(slist) == 0, 'Length must be 0'
+    assert main.Student.numofStudent == 0, 'Class variable numofStudent must be 0'
+
+    print(f'\n**** Total number of students: {main.Student.numofStudent}')
+    for s in slist:
+        print(s)
